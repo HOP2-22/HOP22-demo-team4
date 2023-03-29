66336e876fe7731a6014b2499ca9ba5c88d57d91
bayrjavhlan <bayrjavhlan@lab-1-02s-iMac.local>
2023-03-29 18:22:14 +0800

    Context and Container

--- client/src/provider/AuthContext.jsx
+++ client/src/provider/AuthContext.jsx
@@ -3,14 +3,5 @@ import { createContext } from "react";
 export const AuthContext = createContext();
 
 export default function AuthProvider({ children }) {
-  return (
-    <AuthContext.Provider
-      value={{
-        loading,
-        setLoading,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
+  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
 }
