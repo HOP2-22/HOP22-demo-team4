8feeb5fba29522120ecd16c6fbd90d90a6e8fc0a
bayrjavhlan <bayrjavhlan@lab-1-02s-iMac.local>
2023-03-29 19:24:57 +0800

    a

--- client/src/components/Container.jsx
+++ client/src/components/Container.jsx
@@ -1,7 +1,7 @@
 export default function Container({ children }) {
   return (
     <div className="grow w-full h-full flex justify-center">
-      <div className="w-full sm:w-[576px] lg:w-[900px] xl:w-[1100px] 2xl:w-[1180px] 3xl:w-[1330px] h-full bg-red-300">
+      <div className="w-full sm:w-[576px] lg:w-[900px] xl:w-[1100px] 2xl:w-[1180px] 3xl:w-[1330px] h-full">
         {children}
       </div>
     </div>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,27 @@
 import { BooleanContext } from "@/provider/BooleanContext";
 import { useContext, useEffect } from "react";
+import Container from "./Container";
 
 export default function Navbar() {
   return (
     <div className="w-full h-[60px] bg-blue-500 flex items-center justify-between px-10">
-      <p>Account Trader</p>
-      <div className="flex gap-4 items-center">
-        <div className="w-[80px] py-[10px] rounded-[10px] bg-green-500 text-white flex items-center justify-center cursor-pointer">
-          Log in
+      <Container>
+        <div className="w-full flex justify-between items-center">
+          <div className="">
+            <div className="">Cart</div>
+            <div className="">Favorite</div>
+          </div>
+          <p>Account Trader</p>
+          <div className="flex gap-4 items-center">
+            <div className="w-[80px] py-[10px] rounded-[10px] bg-green-500 text-white flex items-center justify-center cursor-pointer">
+              Log in
+            </div>
+            <div className="w-[80px] py-[10px] rounded-[10px] bg-green-500 text-white flex items-center justify-center cursor-pointer">
+              Sign up
+            </div>
+          </div>
         </div>
-        <div className="w-[80px] py-[10px] rounded-[10px] bg-green-500 text-white flex items-center justify-center cursor-pointer">
-          Sign up
-        </div>
-      </div>
+      </Container>
     </div>
   );
 }
