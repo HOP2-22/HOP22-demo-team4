61e6e6ed9f11bee14ccc5502eaa68a14b140e1c2
Medku0964 <medehguihun@gmail.com>
2023-05-03 19:04:12 +0800

    cards

--- client/src/pages/admin/accounts.jsx
+++ client/src/pages/admin/accounts.jsx
@@ -1,9 +1,36 @@
 import React from "react";
 
 import AdminSideBar from "@/components/admin/AdminSideBar";
+import {
+  Box,
+  ImageListItem,
+  ImageListItemBar,
+  IconButton,
+  Grid,
+} from "@mui/material";
+import InfoIcon from "@mui/icons-material/Info";
+import { AdminAccountImage } from "@/components/admin/Accounts";
+import Link from "next/link";
 
-const Accounts = () => {
-  return <AdminSideBar>Accounts</AdminSideBar>;
+const Accounts = ({ data }) => {
+  console.log(data);
+  return (
+    <AdminSideBar>
+      <AdminAccountImage data={data} />
+    </AdminSideBar>
+  );
 };
 
 export default Accounts;
+
+export async function getServerSideProps(context) {
+  const id = context.query.account;
+
+  const res = await fetch(`http://localhost:8000/api/v1/account/`);
+
+  const data = await res.json();
+
+  return {
+    props: { data: data.data },
+  };
+}
