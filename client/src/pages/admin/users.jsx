7ffd07f3b20aed1d4d5b7452bd0bc6c4ab4debbc
Medku0964 <medehguihun@gmail.com>
2023-05-15 19:43:31 +0800

    connecting

--- client/src/pages/admin/users.jsx
+++ client/src/pages/admin/users.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import axios from "axios";
 
 import AdminSideBar from "@/components/admin/AdminSideBar";
+import { Box } from "@mui/material";
+import Data from "@/components/admin/users/userData";
+import UserData from "@/components/admin/users/UserData";
 
-const Users = () => {
-  return <AdminSideBar>Users</AdminSideBar>;
+const Users = ({ data }) => {
+  console.log(data);
+  return (
+    <AdminSideBar>
+      <Box sx={{ padding: "70px" }}>
+        {/* <UserData data={data} /> */}
+        <Data data={data} />
+      </Box>
+    </AdminSideBar>
+  );
 };
 
 export default Users;
+
+export async function getServerSideProps() {
+  const res = await axios.get(`http://localhost:8000/api/v1/user`);
+
+  return {
+    props: { data: res.data.data },
+  };
+}
