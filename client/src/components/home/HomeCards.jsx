1f26c2df4c8f0ba2ce2aeba3b3d2e911cf6881f4
jawhaa1224 <jawkhlan626@gmail.com>
2023-05-30 13:05:48 +0800

    a

--- client/src/components/home/HomeCards.jsx
+++ client/src/components/home/HomeCards.jsx
@@ -1,11 +1,28 @@
+import { useContext } from "react";
+import Link from "next/link";
+
+import { AuthContext } from "@/provider/AuthContext";
 import { HomeCard } from "./HomeCard";
 
+import { Plus } from "lucide-react";
+
 export const HomeCards = ({ categories }) => {
+  const { handleToTop } = useContext(AuthContext);
+
   return (
     <div className="px-5 sm:px-0 grid grid-cols-2 xsm:grid-cols-3 gap-4 lg:gap-5 sm:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 2xl:grid-cols-8 3xl:grid-cols-9">
       {categories?.map((item, index) => {
         return <HomeCard key={index} data={item} />;
       })}
+      <Link
+        href={"/request"}
+        className="group w-full rounded-[15px] h-[230px] xsm:h-[210px] sm:h-[170px] lg:h-[200px] xl:h-[210px] 3xl:h-[230px] cursor-pointer flex justify-center items-center bg-[#027ffe]"
+        onClick={() => {
+          handleToTop();
+        }}
+      >
+        <Plus size={60} color="white" />
+      </Link>
     </div>
   );
 };
